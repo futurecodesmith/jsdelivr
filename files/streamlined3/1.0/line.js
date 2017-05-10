44ce565997bf6088ed2db503cc9d0c624dfe8174
futurecodesmith <futurecodesmith@users.noreply.github.com>
2017-05-10 11:14:49 -0700

    add files

--- files/streamlined3/1.0/line.js
+++ files/streamlined3/1.0/line.js
@@ -1,83 +1,112 @@
+(function() {
+let socket = io.connect();
 
-var socket = io.connect();
+//set initial SVG params
+let margin = { top: 25, right: 20, bottom: 25, left: 20 };
+let width = 700 - margin.left - margin.right;
+let height = 500 - margin.top - margin.bottom;
 
-let queue = [];
-let data = [];
-let count = 0;
 
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-var width = 700 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+//array to compare incoming data >> if data is the same, do not rerender
+let currData = [];
 
-var svg = d3.select('.chart')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+//draw initial blank graph placeholder
+drawAxis(
+  d3.scaleLinear().domain([0, 20]).range([0, width]), 
+  d3.scaleLinear().domain([0, 40]).range([height, 0]),
+  [{
+    setWidth: 700,
+    setHeight: 500,
+    xTicks: 10,
+    yTicks: 10,
+    xLabel_text: '',
+    yLabel_text: '',
+  }]
+  );
 
-//-----------------------------------------------------------
-socket.on('sendStreamData', (allData) => {
+socket.on('sendLineData', (allData) => {
+  
+  //console.log('ALL DATA: ', allData);
 
-    queue = queue.concat(allData);
+  //if data is not empty or data is new...
+  if (allData.length > 0 || (currData.length > 0 && allData[allData.length - 1].xScale !== currData[currData.length - 1].xScale)) {
 
-    if (allData.length >= 50) {
-      allData = allData.slice(-49);
+    currData = allData;
+
+    width = allData[0].setWidth - margin.left - margin.right;
+    height = allData[0].setHeight - margin.top - margin.bottom;
+
+    let xScale;
+
+    if (allData[0].shiftXAxis) {
+      xScale = d3.scaleLinear()
+        .domain([
+          d3.min(allData, d => d.xScale),
+          Math.max(allData[0].xDomainUpper, d3.max(allData, d => d.xScale))
+        ])
+        .range([0, width]);
+
+    } else {
+      xScale = d3.scaleLinear()
+        .domain([allData[0].xDomainLower, allData[0].xDomainUpper])
+        .range([0, width]);
     }
-if(allData) {
 
-  var xScale = d3.scaleLinear()
-    // .domain([0, 200])
-    .domain([
-      d3.min(allData, d => d.xScale),
-      Math.max(50, d3.max(allData, d => d.xScale))
-    ])
-    .range([0, width]);
+    let yScale = d3.scaleLinear()
+      .domain([allData[0].yDomainLower, allData[0].yDomainUpper])
+      .range([height, 0]);
+
+    let line = d3.line()
+      .x(d => xScale(d.xScale))
+      .y(d => yScale(d.yScale))
+    
+    drawAxis(xScale, yScale, allData);
+    drawContent(line, allData);
+  };
+})
+
+function drawAxis(xScale, yScale, allData) {
+
+  d3.select('#lineSVG').remove();
+
+  svg = d3.select('#line-chart')
+    .append('svg')
+    .attr('id', 'lineSVG')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('class', 'mount')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
   svg
     .append('g')
     .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale).ticks(allData[0].xTicks));
 
-  svg
-    .select('g')
-    .call(d3.axisBottom(xScale).ticks(10));
-
-  // Add the text label for the x axis
   svg.append("text")
     .attr('transform', 'translate(' + (width) + ' ,' + (height + margin.bottom) + ')')
     .style('text-anchor', 'end')
     .style('font-family', 'sans-serif')
     .style('font-size', '13px')
-    .text('');
-
-  var yScale = d3.scaleLinear()
-    .domain([0, 35])
-    .range([height, 0]);
+    .text(allData[0].xLabel_text);
 
   svg
     .append('g')
-    .attr('class', 'yAxis');
-  
-  svg.select('.yAxis')
-    .call(d3.axisLeft(yScale).ticks(10));
+    .attr('class', 'yAxis')
+    .call(d3.axisLeft(yScale).ticks(allData[0].yTicks));
 
   svg.append("text")
-        .attr("transform", "rotate(0)")
-        .attr("y",-10)
-        .attr("x", -40)
-        .attr("dy", "1em")
-        .attr('class', 'yLabel')
-        .style("text-anchor", "end")
-        .style('font-family', 'sans-serif')
-        .style('font-size', '13px')
-        .text("");
+    .attr("transform", "rotate(-90)")
+    .attr("y", -50)
+    .attr("x", 0)
+    .attr("dy", "1em")
+    .style("text-anchor", "end")
+    .style('font-family', 'sans-serif')
+    .style('font-size', '13px')
+    .text(allData[0].yLabel_text);
+}
 
-  var line = d3.line()
-    .x(d => xScale(d.xScale))
-    .y(d => yScale(d.yScale))
-    //.curve(d3.curveCatmullRom.alpha(.5));
-
-  d3.selectAll('path.line').remove();
-  d3.selectAll('.dot').remove();
+function drawContent(line, allData) {
 
   svg
     .selectAll('.line')
@@ -89,13 +118,12 @@ if(allData) {
     .style('stroke', '#5176B6')
     .style('stroke-width', 1)
     .style('fill', 'none')
-    .style('stroke-linejoin','round');
+    .style('stroke-linejoin', 'round');
 
-
-svg.selectAll('.dot')
-  .data(allData)
-  .enter()
-  .append('circle')
+  svg.selectAll('.dot')
+    .data(allData)
+    .enter()
+    .append('circle')
     .attr('class', 'dot')
     .attr('cx', line.x())
     .attr('cy', line.y())
@@ -104,17 +132,4 @@ svg.selectAll('.dot')
     .style('stroke-width', 1.5)
     .style('stroke', 'DodgerBlue');
 }
-
-})
-
-setInterval(() => {
-  if(queue.length > 1) {
-    data.push(queue[count]);
-    count++;
-  }
-}, 1000);
-
-
-
- 
-
+})();
